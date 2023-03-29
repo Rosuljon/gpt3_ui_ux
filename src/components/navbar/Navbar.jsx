c10efed349650cb7a9ad58a3b0f2578a9baaf32b
Ronny <ikramjanov97@mail.ru>
2023-03-29 15:25:34 +0900

    added assets and started navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons'
 import './navbar.css'
+import logo from "../../assets/logo.svg"
+// BEM => Block Element Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'> 
+          <div className='gpt3__navbar-links_logo'>
+            <img src={logo} alt="logo" />
+          </div>
+          <div className='gpt3__navbar-links_container'>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
+          </div>
+      </div>
+      </div>
   )
 }
 
